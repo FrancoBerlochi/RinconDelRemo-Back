fc6107e3edf0215d1da2cc18a13308007938fb7c
MarianoGerosa <gerosamariano@gmail.com>
2025-05-22 14:14:33 -0300

    Agrego herencias y hago inyeccion en AttendantRepository

--- src/Infrastructure/Data/Repositories/AttendantRepository.cs
+++ src/Infrastructure/Data/Repositories/AttendantRepository.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Domain.Entities;
 using Domain.Interfaces;
 
 namespace Infrastructure.Data.Repositories
 {
-    public class AttendantRepository : IAttendantRepository
+    public class AttendantRepository : BaseRepository<Attendant>, IAttendantRepository
     {
-
+        private readonly ApplicationContext _context;
+        public AttendantRepository(ApplicationContext context) : base(context)
+        {
+            _context = context;
+        }
     }
 }
