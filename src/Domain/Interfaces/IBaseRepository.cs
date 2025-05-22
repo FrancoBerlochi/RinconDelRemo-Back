efc821e218a1807f14f8f5135e37a07e968c402c
MarianoGerosa <gerosamariano@gmail.com>
2025-05-22 13:36:19 -0300

    Completo IBaseRepository

--- src/Domain/Interfaces/IBaseRepository.cs
+++ src/Domain/Interfaces/IBaseRepository.cs
@@ -3,6 +3,12 @@ namespace Domain.Interfaces
 {
     public interface IBaseRepository<T> where T : class
     {
+        T? GetById(int id);
+        List<T> GetAll();
+        T Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void SaveChanges();
 
     }
 }
