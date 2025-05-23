2009e6081da90a43a1a1aac67af6d3804d19f2a0
ivanapanowicz <ivanapanowicz@gmail.com>
2025-05-23 15:10:28 -0300

    completo DTO de dueño

--- src/Application/Models/OwnerDto.cs
+++ src/Application/Models/OwnerDto.cs
@@ -8,6 +8,11 @@ namespace Application.Models
 {
     public class OwnerDto
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
 
     }
 }
