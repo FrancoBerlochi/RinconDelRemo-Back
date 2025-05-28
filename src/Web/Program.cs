b6b0c2c4f18d5be5bdfbbb5c14aa90aca405827d
Franco Berlochi <francoberlochi@gmail.com>
2025-05-28 18:06:08 -0300

    azure config

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -2,6 +2,7 @@ using Application.Interfaces;
 using Application.Services;
 using Domain.Interfaces;
 using Infrastructure.Data.Repositories;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,52 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+#region Swagger config
+//builder.Services.AddSwaggerGen();
+string instance = builder.Configuration["AzureAd:Instance"]!;
+string tenantId = builder.Configuration["AzureAd:TenantId"]!;
+string clientid = builder.Configuration["AzureAd:ClientId"]!;
+string ApplicationIdURI = builder.Configuration["AzureAd:ApplicationIdURI"]!;
+string scope = "default";
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+    // Configure OAuth2
+    c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows
+        {
+            AuthorizationCode = new OpenApiOAuthFlow
+            {
+                AuthorizationUrl = new Uri($"{instance}{tenantId}/oauth2/v2.0/authorize"),
+                TokenUrl = new Uri($"{instance}{tenantId}/oauth2/v2.0/token"),
+                Scopes = new Dictionary<string, string>
+                {
+                    { $"{ApplicationIdURI}/{scope}", "Access API" }
+                }
+            }
+        }
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "oauth2"
+                }
+            },
+            new[] { $"{ApplicationIdURI}/{scope}" }
+        }
+    });
+});
+#endregion
+
 #region Services
 builder.Services.AddScoped<IKayakService, KayakService>();
 
@@ -20,6 +67,7 @@ builder.Services.AddScoped<IKayakService, KayakService>();
 #region Repositories
 builder.Services.AddScoped<IKayakRepository, KayakRepository>();
 #endregion
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
