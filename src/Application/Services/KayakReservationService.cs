0cc3a7617fc7cb6e4f4536473c407b70a6495835
Valentino <valentinomaffeis02@gmail.com>
2025-06-08 11:45:56 -0300

    crea CRUD, DTO, DTO create y update request, inyeccion completa en service de KayakReservation

---
+++ src/Application/Services/KayakReservationService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Interfaces;
+using Application.Models;
+using Application.Models.Request;
+using Domain.Entities;
+using Domain.Enums;
+using Domain.Exceptions;
+using Domain.Interfaces;
+
+namespace Application.Services
+{
+    public class KayakReservationService : IKayakReservationService
+    {
+        public readonly IKayakReservationRepository _kayakReservationRepository;
+        public KayakReservationService (IKayakReservationRepository kayakReservationRepository)
+        {
+            _kayakReservationRepository = kayakReservationRepository;
+        }
+
+        public List<KayakReservationDto> GetAll()
+        {
+            var reservas = _kayakReservationRepository.GetAll();
+            return reservas.Select(KayakReservationDto.Create).ToList();
+        }
+        public KayakReservationDto? GetById(int id)
+        {
+            var reserva = _kayakReservationRepository.GetById(id) ?? throw new NotFoundException("Reserva no encontrada.");
+            return KayakReservationDto.Create(reserva);
+        }
+        public KayakReservation Create(KayakReservationCreateRequest request)
+        {
+            if (request.FechaFin <= request.FechaInicio)
+            {
+                throw new Exception("La fecha de fin debe ser posterior a la fecha de inicio.");
+            }
+            var reservation = new KayakReservation();
+            reservation.KayakId = request.KayakId;
+            reservation.TenantId = request.TenantId;
+            reservation.FechaInicio = request.FechaInicio;
+            reservation.FechaFin = request.FechaFin;
+            reservation.StatusReservation = StatusReservation.Active;
+            _kayakReservationRepository.Create(reservation);
+            return reservation;
+        }
+
+        public void Update(int id, KayakReservationUpdateRequest request)
+        { 
+            var reserva = _kayakReservationRepository.GetById(id) ?? throw new NotFoundException("reserva no encontrada.");
+            reserva.FechaInicio= request.FechaInicio;
+            reserva.FechaFin= request.FechaFin;
+            reserva.KayakId= request.KayakId;
+
+            _kayakReservationRepository.Update(reserva);
+
+        }
+        public void Delete(int id)
+        {
+            var reserva = _kayakReservationRepository.GetById(id) ?? throw new NotFoundException("Reserva no encontrada.");
+            _kayakReservationRepository.Delete(reserva);
+        }
+    }
+}
